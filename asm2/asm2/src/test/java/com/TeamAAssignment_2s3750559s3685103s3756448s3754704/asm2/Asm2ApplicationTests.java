fecc50c0bc36daf082ad89742bf06137865d5fa2
Shin <s3750559@rmit.edu.vn>
2022-05-08 17:41:53 +0700

    Upload Spring
    Spring Web, Lombok, Spring Boot DevTools

---
+++ asm2/asm2/src/test/java/com/TeamAAssignment_2s3750559s3685103s3756448s3754704/asm2/Asm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeamAAssignment_2s3750559s3685103s3756448s3754704.asm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
